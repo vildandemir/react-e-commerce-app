5054deb613fcf01f1985507b95c164a0c8f8e7ca
Vildan Demir <vildandemir000@gmail.com>
2021-11-16 21:07:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Home}>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
+function Home() {
+  return <div>Home</div>;
+}
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,27 @@
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.left}>
+        <div className="logo">
+          <Link to="/">E-Commerce</Link>
+        </div>
+        <ul>
+          <li className={styles.menu}>
+            <Link to="/">Products</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="right">
+        <Button colorScheme="blue" size="sm">
+          LOGIN
+        </Button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
