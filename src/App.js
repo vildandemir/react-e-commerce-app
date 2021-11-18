dc3a3d990655f89fc32e7c3a5a6b4d63e8768cc2
Vildan Demir <vildandemir000@gmail.com>
2021-11-19 00:25:40 +0300

    fix router and import components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
+import Products from "./components/Products";
+import Cart from "./components/Cart/index";
+import Favorites from "./components/Favorites";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
+        <Products />
         <Switch>
-          <Route path="/" component={Home}>
-            <Home />
-          </Route>
+          <Route path="/" exact component={Products} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/favorites" component={Favorites} />
         </Switch>
       </div>
     </Router>
   );
 }
-function Home() {
-  return <div>Home</div>;
-}
+
 export default App;
