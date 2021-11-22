6d5e697bcb73fe4f7c9e74b5e23f964a01f6b51e
Vildan Demir <vildandemir000@gmail.com>
2021-11-23 00:42:14 +0300

    use params, get data and show product details

--- src/components/Product Details/index.js
+++ src/components/Product Details/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import { Box, Image, Badge, Grid, Button } from "@chakra-ui/react";
+import Card from "../Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function ProductDetails() {
+  const [data, setData] = useState([]);
+
+  const { product_id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://6196420eaf46280017e7df35.mockapi.io/api/clothes/${product_id}`
+      );
+      setData(response.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       Product Details
+      <Box>
+        {product_id}
+        <Image src={data.image}></Image>
+      </Box>
       <Box>
         <Button colorScheme="yellow" color="black" size="sm" ml={5}>
           Add to Cart
