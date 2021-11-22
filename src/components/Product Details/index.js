d8a0e430b10a4c54839770910ac1265da9c1f113
Vildan Demir <vildandemir000@gmail.com>
2021-11-22 22:12:57 +0300

    add product details page

--- src/components/Product Details/index.js
+++ src/components/Product Details/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, Image, Badge, Grid, Button } from "@chakra-ui/react";
 
 function ProductDetails() {
-  return <div>Product Details</div>;
+  return (
+    <div>
+      Product Details
+      <Box>
+        <Button colorScheme="yellow" color="black" size="sm" ml={5}>
+          Add to Cart
+        </Button>
+        <Button colorScheme="pink" color="black" size="sm" ml={5}>
+          Add to Favorites
+        </Button>
+        <Button colorScheme="pink" color="black" size="sm" ml={5}>
+          Back to Homepage
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
 export default ProductDetails;
