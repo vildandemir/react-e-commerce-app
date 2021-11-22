24270b0a89e1e889185dee35b8dbcb2baebdc14a
Vildan Demir <vildandemir000@gmail.com>
2021-11-22 22:14:28 +0300

    add categories menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,20 @@
 import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
+import { ChevronDownIcon, StarIcon, ArrowRightIcon } from "@chakra-ui/icons";
 import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
+} from "@chakra-ui/react";
+import { MdShoppingCart, MdFavorite } from "react-icons/md";
 
 function Navbar() {
   return (
@@ -15,12 +29,33 @@ function Navbar() {
           </li>
         </ul>
       </div>
+      <ul>
+        <li className="categories">
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Categories
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Jackets</MenuItem>
+              <MenuItem>Pants</MenuItem>
+              <MenuItem>T-Shirts</MenuItem>
+              <MenuItem>Bags</MenuItem>
+              <MenuItem>Eye Glasses</MenuItem>
+            </MenuList>
+          </Menu>
+        </li>
+      </ul>
       <div className="right">
         <Button colorScheme="pink" color="black" size="md">
-          <Link to="/favorites">Favorites</Link>
+          <Link to="/favorites">
+            <MdFavorite w={8} h={8} ml={3} />
+          </Link>
         </Button>
-        <Button colorScheme="yellow" color="black" size="md">
-          <Link to="/cart">Cart</Link>
+
+        <Button colorScheme="yellow" color="black" size="md" ml={5}>
+          <Link to="/cart">
+            <MdShoppingCart w={8} h={8} ml={3} color="black" />
+          </Link>
         </Button>
       </div>
     </nav>
