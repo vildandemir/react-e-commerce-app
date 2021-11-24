e398b23aed38e3e049bc26a13dd12af4340b0ce5
Vildan Demir <vildandemir000@gmail.com>
2021-11-25 01:33:19 +0300

    list products in cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useCart } from "../../context/CartContext";
+import Card from "../Card/index";
 
 function Cart() {
-  return <div>Cart</div>;
+  const { items } = useCart();
+  console.log(items);
+  return (
+    <div>
+      {items.map((item, key) => (
+        <Card key={key} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default Cart;
