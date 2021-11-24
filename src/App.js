420ca0beb2cc3e71ba43a2325e4852f8eeb61cbc
Vildan Demir <vildandemir000@gmail.com>
2021-11-25 00:54:17 +0300

    add cart context provider

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Products from "./components/Products";
 import Cart from "./components/Cart/index";
 import Favorites from "./components/Favorites";
 import ProductDetails from "./components/Product Details";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Products} />
-          <Route path="/product/:product_id" component={ProductDetails} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/favorites" component={Favorites} />
-        </Switch>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Products} />
+            <Route path="/product/:product_id" component={ProductDetails} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/favorites" component={Favorites} />
+          </Switch>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
