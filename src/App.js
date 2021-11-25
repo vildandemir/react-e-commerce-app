913da2ad0b5b5d2ca4a18d13070faf105356aa8a
Vildan Demir <vildandemir000@gmail.com>
2021-11-25 22:21:20 +0300

    add Favorites Context Provider

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import Cart from "./components/Cart/index";
 import Favorites from "./components/Favorites";
 import ProductDetails from "./components/Product Details";
 import { CartProvider } from "./context/CartContext";
+import { FavoritesProvider } from "./context/FavoritesContext";
 
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <div>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={Products} />
-            <Route path="/product/:product_id" component={ProductDetails} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/favorites" component={Favorites} />
-          </Switch>
-        </div>
-      </Router>
+      <FavoritesProvider>
+        <Router>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={Products} />
+              <Route path="/product/:product_id" component={ProductDetails} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/favorites" component={Favorites} />
+            </Switch>
+          </div>
+        </Router>
+      </FavoritesProvider>
     </CartProvider>
   );
 }
