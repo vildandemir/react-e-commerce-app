879468dd2b5ecd8ef79bfad7c43880398f29a654
Vildan Demir <vildandemir000@gmail.com>
2021-11-26 01:21:57 +0300

    calculate the total cart amount and run remove item from cart func

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useCart } from "../../context/CartContext";
 import Card from "../Card/index";
-import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-} from "@chakra-ui/react";
+import { Alert, AlertIcon } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Image, Button, Text } from "@chakra-ui/react";
 
 function Cart() {
-  const { items } = useCart();
+  const { items, removeItemFromCart } = useCart();
   console.log(items);
+
+  const totalPrice = items.reduce((acc, obj) => acc + obj.price, 0);
+
   return (
     <div>
       {items.length < 1 && (
@@ -19,8 +19,30 @@ function Cart() {
           You have not any product in your cart.
         </Alert>
       )}
-      {items.length > 0 &&
-        items.map((item, key) => <Card key={key} item={item} />)}
+      {items.length > 0 && (
+        <>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                <Link to={`/product/${item.id}`}>
+                  <Image htmlWidth="200" src={item.image} alt="cart item" />
+                  {item.name} - {item.price}
+                </Link>
+                <Button
+                  onClick={() => removeItemFromCart(item.id)}
+                  colorScheme="pink"
+                  color="black"
+                  size="sm"
+                  ml={5}
+                >
+                  Remove From Cart
+                </Button>
+              </li>
+            ))}
+          </ul>
+          <Text>Total : {totalPrice} $</Text>
+        </>
+      )}
     </div>
   );
 }
