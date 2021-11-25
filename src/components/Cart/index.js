09053027e99826eba7ccf420fea539c71839d792
Vildan Demir <vildandemir000@gmail.com>
2021-11-25 22:28:53 +0300

    add alert and list products

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { useCart } from "../../context/CartContext";
 import Card from "../Card/index";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 
 function Cart() {
   const { items } = useCart();
   console.log(items);
   return (
     <div>
-      {items.map((item, key) => (
-        <Card key={key} item={item} />
-      ))}
+      {items.length < 1 && (
+        <Alert status="warning">
+          <AlertIcon />
+          You have not any product in your cart.
+        </Alert>
+      )}
+      {items.length > 0 &&
+        items.map((item, key) => <Card key={key} item={item} />)}
     </div>
   );
 }
--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useFavorites } from "../../context/FavoritesContext";
+import Card from "../Card";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 
 function Favorites() {
-  return <div>Favorites</div>;
+  const { products } = useFavorites();
+  console.log(products);
+  return (
+    <div>
+      {products.length < 1 && (
+        <Alert status="warning">
+          <AlertIcon />
+          You have not any product in your favorite list.
+        </Alert>
+      )}
+      {products.length > 0 &&
+        products.map((item, key) => <Card key={key} item={item} />)}
+    </div>
+  );
 }
 
 export default Favorites;
