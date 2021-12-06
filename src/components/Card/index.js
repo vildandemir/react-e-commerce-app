ac6136d3d2f409a1dc3d4adecc58b9ba22d4b446
Vildan Demir <vildandemir000@gmail.com>
2021-12-06 20:51:19 +0300

    update card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,17 @@
 import { Box, Image, Badge, Grid, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { StarIcon } from "@chakra-ui/icons";
+import { BsTruck } from "react-icons/bs";
 
 function Card({ item }) {
   return (
     <div>
       <Box key={item.id} w="100%" h="400" bg="blue.200">
         <Link to={`/product/${item.id}`}>
-          <Image src={item.image}></Image>
+          <Image src={item.image} boxSize="200px"></Image>
           <Box>{item.name}</Box>
           <Box>{item.color}</Box>
-          <Box>{item.price}</Box>
+          <Box>{item.price} $ </Box>
           <Box display="flex" mt="2" alignItems="center">
             {Array(5)
               .fill("")
@@ -20,6 +21,14 @@ function Card({ item }) {
                   color={i < item.rating ? "teal.500" : "gray.300"}
                 />
               ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {item.reviewCount} reviews
+            </Box>
+          </Box>
+          <br />
+          <Box as="span" ml="5" color="gray.600" fontSize="md">
+            <BsTruck ml="36" />
+            Shipping within 3 days
           </Box>
         </Link>
       </Box>
