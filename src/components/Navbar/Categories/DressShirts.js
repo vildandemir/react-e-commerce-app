ae133ab923475f282d1f5cc04f776455f65ab797
Vildan Demir <vildandemir000@gmail.com>
2021-12-06 20:52:32 +0300

    create dress shirts component and list dress shirt products

---
+++ src/components/Navbar/Categories/DressShirts.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "../../Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Grid } from "@chakra-ui/react";
+
+function DressShirts() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://6196420eaf46280017e7df35.mockapi.io/api/clothes"
+      );
+      setData(response.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Box>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          {data
+
+            .filter((item) => item.categories.includes("dressshirt"))
+            .map((item, key) => (
+              <Card key={key} item={item} />
+            ))}
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
+export default DressShirts;
+{
+  /* <Card key={key} item={item} />
+.filter((item,key)=>(item.categories==="dressshirt"
+<Card key={key} item={item} />))
+} */
+}
