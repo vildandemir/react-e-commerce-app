b086581046d733eda2944da311bf549d10b05526
Vildan Demir <vildandemir000@gmail.com>
2021-12-29 00:56:47 +0300

    create functions and methods for authentication display on screen

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -12,78 +12,208 @@ import {
   Heading,
   Text,
   useColorModeValue,
-  Link,
 } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../../../src/firebase-config";
+import Login from "./Login";
 
 export default function Auth() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [user, setUser] = useState({});
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  const login = async () => {};
+
+  const logout = async () => {};
+
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Stack spacing={4} mx={"auto"} minW={"md"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"} textAlign={"center"}>
-            Sign up
-          </Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            to do all your shopping ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow={"lg"}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? "text" : "password"} />
-                <InputRightElement h={"full"}>
-                  <Button
-                    variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
-                  >
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={"yellow.400"}
-                color={"white"}
-                _hover={{
-                  bg: "yellow.500",
-                }}
-              >
+    <div>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        {!user && (
+          <Stack spacing={4} mx={"auto"} minW={"md"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"4xl"} textAlign={"center"}>
                 Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={"center"}>
-                Already a user? <Link color={"blue.400"}>Login</Link>
+              </Heading>
+              <Text fontSize={"lg"} color={"gray.600"}>
+                to do all your shopping ✌️
               </Text>
             </Stack>
+            <Box rounded={"lg"} bg={"white"} boxShadow={"lg"} p={8}>
+              <Stack spacing={4}>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    type="email"
+                    onChange={(e) => {
+                      setRegisterEmail(e.target.value);
+                    }}
+                  />
+                </FormControl>
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      onChange={(e) => {
+                        setRegisterPassword(e.target.value);
+                      }}
+                    />
+                    <InputRightElement h={"full"}>
+                      <Button
+                        variant={"ghost"}
+                        onClick={() =>
+                          setShowPassword((showPassword) => !showPassword)
+                        }
+                      >
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    loadingText="Submitting"
+                    size="lg"
+                    bg={"yellow.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "yellow.500",
+                    }}
+                    onClick={register}
+                  >
+                    Sign up
+                  </Button>
+                </Stack>
+
+                <Stack pt={6}>
+                  <Text align={"center"}>
+                    Already a user?{" "}
+                    <Link color={"blue.400"} to="/login">
+                      Login
+                    </Link>
+                  </Text>
+                </Stack>
+              </Stack>
+            </Box>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+        )}
+        {user && (
+          <Stack spacing={4} mx={"auto"} minW={"md"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"4xl"} textAlign={"center"}>
+                Sign in
+              </Heading>
+              <Text fontSize={"lg"} color={"gray.600"}>
+                to do all your shopping ✌️
+              </Text>
+            </Stack>
+            <Box rounded={"lg"} bg={"white"} boxShadow={"lg"} p={8}>
+              <Stack spacing={4}>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder={user.email}
+                    onChange={(e) => {
+                      setRegisterEmail(e.target.value);
+                    }}
+                  />
+                </FormControl>
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      onChange={(e) => {
+                        setRegisterPassword(e.target.value);
+                      }}
+                      placeholder={user.password}
+                    />
+                    <InputRightElement h={"full"}>
+                      <Button
+                        variant={"ghost"}
+                        onClick={() =>
+                          setShowPassword((showPassword) => !showPassword)
+                        }
+                      >
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    loadingText="Submitting"
+                    size="lg"
+                    bg={"yellow.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "yellow.500",
+                    }}
+                    onClick={register}
+                  >
+                    <Link to="/">Login</Link>
+                  </Button>
+                </Stack>
+
+                {user && (
+                  <Stack pt={6}>
+                    <Text align={"center"}>User Logged In {user.email}</Text>
+                    <Button
+                      loadingText="Submitting"
+                      size="sm"
+                      bg={"yellow.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "yellow.500",
+                      }}
+                    >
+                      Sign Out
+                    </Button>
+                  </Stack>
+                )}
+              </Stack>
+            </Box>
+          </Stack>
+        )}
+      </Flex>
+    </div>
   );
 }
